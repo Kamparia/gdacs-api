a8034a4cb6b1f04930e84dda3095d662c16cb339
Olaoye Somide <somideolaoye@gmail.com>
2021-07-01 14:34:10 +0200

    Deploy new package version

--- gdacs/api.py
+++ gdacs/api.py
@@ -1,3 +1,4 @@
+import os
 import json
 import requests
 import xmltodict
@@ -22,13 +23,19 @@ class GDACSAPIReader:
         return "GDACS API Client."
 
     @cached(cache=TTLCache(maxsize=500, ttl=CACHE_TTL))
-    def latest_events(self, event_type:str=None, historical:str='default', limit:int=None):
+    def latest_events(self, 
+        event_type:str=None, 
+        historical:str='default', 
+        limit:int=None
+    ):
         """ Get latest events from GDACS RSS feed. """
         if event_type not in EVENT_TYPES:
-            raise GDACSAPIError("API Error: Used an invalid `event_type` parameter in request.")
+            error_msg = "API Error: Used an invalid `event_type` parameter in request."
+            raise GDACSAPIError(error_msg)
 
         if historical not in RSS_FEED_URLS.keys():
-            raise GDACSAPIError("API Error: Used an invalid `historical` parameter in request.")
+            error_msg = "API Error: Used an invalid `historical` parameter in request."
+            raise GDACSAPIError(error_msg)
             
         res = requests.get(RSS_FEED_URLS[historical])
         if res.status_code == 200:
@@ -42,17 +49,26 @@ class GDACSAPIReader:
                 events.append(item)
             return json.dumps(events[:limit])
         else:
-            raise GDACSAPIError("API Error: GDACS RSS feed can not be reached.")
+            error_msg = "API Error: GDACS RSS feed can not be reached."
+            raise GDACSAPIError(error_msg)
+
 
     @cached(cache=TTLCache(maxsize=500, ttl=CACHE_TTL))
-    def get_event(self, event_type: str, event_id: str, episode_id: str=None, source_format: str='xml', cap_file: bool=False):
+    def get_event(self, 
+        event_type: str, 
+        event_id: str, 
+        episode_id: str=None, 
+        source_format: str='xml', 
+        cap_file: bool=False
+    ):
         """ Get record of a single event from GDACS API. """
         def handle_geojson(endpoint):
             res = requests.get(endpoint)
             if  res.status_code == 200:
                 return json.dumps(json.loads(res.content))
             else:
-                raise GDACSAPIError("API Error: Unable to read GeoJSON data for GDACS event.")
+                error_msg = "API Error: Unable to read GeoJSON data for GDACS event."
+                raise GDACSAPIError(error_msg)
 
         def handle_xml(endpoint):
             res = requests.get(endpoint)
@@ -61,7 +77,8 @@ class GDACSAPIReader:
                 content = xml_parser["rss"]["channel"]["item"]
                 return json.dumps(content)
             else:
-                raise GDACSAPIError("API Error: Unable to read XML data for GDACS event.")
+                error_msg = "API Error: Unable to read XML data for GDACS event."
+                raise GDACSAPIError(error_msg)
 
         def download_shp(endpoint):
             res = requests.get(endpoint, allow_redirects=True)
@@ -70,29 +87,66 @@ class GDACSAPIReader:
                     shp_file_name = endpoint.split('/')[-1]
                     with open(shp_file_name, 'wb') as download:
                         download.write(res.content)
-                        return f"Downloaded {shp_file_name} in directory."
+                        return "Downloaded {} in directory.".format(shp_file_name)
                 except Exception as error:
                     raise error
             else:
-                raise GDACSAPIError("API Error: Unable to read SHP/KML data for GDACS event.")
+                error_msg = "API Error: Unable to read SHP/KML data for GDACS event."
+                raise GDACSAPIError(error_msg)
         
         if event_type not in EVENT_TYPES:
-            raise GDACSAPIError("API Error: Used an invalid `event_type` parameter in request.")
+            error_msg = "API Error: Used an invalid `event_type` parameter in request."
+            raise GDACSAPIError(error_msg)
 
         if source_format not in DATA_FORMATS:
-            raise GDACSAPIError("API Error: Used an invalid `data_format` parameter in request.")
+            error_msg = "API Error: Used an invalid `data_format` parameter in request."
+            raise GDACSAPIError(error_msg)
 
         if source_format == 'geojson':
-            return handle_geojson(f"{BASE_URL}/{event_type}/{event_id}/geojson_{event_id}_{episode_id}.geojson")
+            url_path = os.path.join(
+                BASE_URL, 
+                event_type, 
+                event_id, 
+                "geojson_{}_{}.geojson".format(event_id, episode_id)
+            )
+            return handle_geojson(url_path)
+
         elif source_format == 'shp':
-            return download_shp(f"{BASE_URL}/{event_type}/{event_id}/Shape_{event_id}_{episode_id}.zip")
+            url_path = os.path.join(
+                BASE_URL, 
+                event_type, 
+                event_id, 
+                "Shape_{}_{}.zip".format(event_id, episode_id)
+            )
+            return download_shp(url_path)
+
         else:
             if cap_file == True:
-                return handle_xml(f"{BASE_URL}/{event_type}/{event_id}/cap_{event_id}.xml")
+                url_path = os.path.join(
+                    BASE_URL, 
+                    event_type, 
+                    event_id, 
+                    "cap_{}.xml".format(event_id)
+                )
+                return handle_xml(url_path)
+
             elif episode_id == None:
-                return handle_xml(f"{BASE_URL}/{event_type}/{event_id}/rss_{event_id}.xml")
+                url_path = os.path.join(
+                    BASE_URL, 
+                    event_type, 
+                    event_id, 
+                    "rss_{}.xml".format(event_id)
+                )
+                return handle_xml(url_path)
+
             else:
-                return handle_xml(f"{BASE_URL}/{event_type}/{event_id}/rss_{event_id}_{episode_id}.xml")
+                url_path = os.path.join(
+                    BASE_URL, 
+                    event_type, 
+                    event_id, 
+                    "rss_{}_{}.xml".format(event_id, episode_id)
+                )
+                return handle_xml(url_path)
 
 
 class GDACSAPIError(RuntimeError):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 from setuptools import setup
 
-version = "0.1.3"
+version = "0.1.4"
 
 long_description = open('README.md').read()
 
